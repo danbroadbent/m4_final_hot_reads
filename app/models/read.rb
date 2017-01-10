eaaef8a08625f7ec2c7ceadab4161d0314995ca5
Dan Broadbent <dbroadbent85@gmail.com>
2017-01-09 18:13:33 -0700

    Remove top scope from read model

--- app/models/read.rb
+++ app/models/read.rb
@@ -2,7 +2,4 @@ class Read < ApplicationRecord
   scope :hot, -> {
     self.where(updated_at: (Time.now - 24.hours)..Time.now)
         .order(read_count: :desc).limit(10)}
-  scope :top, -> {
-    self.where(updated_at: (Time.now - 24.hours)..Time.now)
-        .order(read_count: :desc).limit(10)}
 end
